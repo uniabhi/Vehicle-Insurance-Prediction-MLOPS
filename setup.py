8d9eebcd94214f6c874a9099efae790a78d0294f
uniabhi <uniabhi5684@gmail.com>
2025-11-07 23:27:27 +0530

    project structure ready, first push, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abhishek_Kumar",
+    author_email="abhishek074.kr@gmail.com",
+    packages=find_packages()
+)
