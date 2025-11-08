f29770bcde34752b1e777c914fdd232bdd52b10a
uniabhi <uniabhi5684@gmail.com>
2025-11-08 12:54:47 +0530

    ML experiment codes added

--- demo.py
+++ demo.py
@@ -8,12 +8,12 @@ logging.error("This is an error message.")
 logging.critical("This is a critical message.")
 
 # # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
